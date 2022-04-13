89ee3cc6b7d557f9b4679a65dee2491160d8aa4d
Eduardo Mendez <joseeduardomendezb@gmail.com>
2022-04-13 15:44:12 -0700

    macOS Monterey config

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  zoxide init fish | source
+	starship init fish | source
+  rbenv init - fish | source
+
+	alias vim="lvim"
+	alias conda="mamba"
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
